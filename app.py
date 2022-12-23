0f3d1c5d1b3a0d9ef5d8874c88391f58d766b619
Viren Gajera <gajeraviren07@gmail.com>
2022-12-23 12:24:51 +0100

    DB support added

--- app.py
+++ app.py
@@ -1,34 +1,46 @@
-""" 
+import json
+import re
 
-1. Add Tracker
-2. View Trackers
-3. Remove Tracker
-4. View Price Only
- """
+def read_db():
+    json_file = open('db.json','r')
+    data = json.load(json_file)
+    json_file.close()
+    return data
 
 
-""" 
- 1. Add Tracker
-    - Enter Link, Threshold value, email
-    - Validate Link
-    - Add Tracker
- 
-"""
+def update_db(data):
+    json_file = open('db.json','w')
+    json.dump(data,json_file)
+    json_file.close()
+    return "Data Updated"
 
-def validate():
-    pass
 
-def add_tracker(link,threshold_value,email):
-    pass
 
-def view_tracker():
+def isValidLink(link):
+    amz = link[:18]
+    if amz == "https://amazon.de/":
+        print("Valid amazon url")
+        return True
+    else:
+        return False
+    print()
+
+def add_tracker(email,link,threshold_value):
+    r_data = read_db()
+    if not(isValidLink(link)):
+        print("Invalid URL")
+    
+
+def view_tracker(email):
     pass
 
 def remove_tracker():
     pass
 
 def view_price():
-    pass
+    link = input("Enter Link Here: ")
+    validate(link)
+    print("Here is the price")
 
 def tracker():
     pass
@@ -49,12 +61,14 @@ def main():
         ch = int(input("Enter Choice: "))
         if ch == 1:
             print("Add Tracker f ")
+            add_tracker("a","b","c")
         elif ch == 2:
             print("View Tracker fn")
         elif ch == 3:
             print("Remove Tracker fn")
         elif ch == 4:
             print("View Price Only")
+            view_price()
         elif ch == 5:
             break
         else:
